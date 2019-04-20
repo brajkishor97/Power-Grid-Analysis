adafb59e5974fcce8118f8b5176a26779ca0b133
Braj Kishor <kumarbrajkishor96@gmail.com>
2019-04-20 16:15:17 +0530

    Remove indentation errors.

--- randomWalkSerial.cpp
+++ randomWalkSerial.cpp
@@ -19,8 +19,8 @@ int main(int argc, char* argv[])
   double *potential;
   if(argc!=2)
   {
-	cout<<"Insufficient arguments"<<endl;
-	return 1;
+    cout<<"Insufficient arguments"<<endl;
+    return 1;
   }
 
   double *rand_array;
@@ -35,20 +35,20 @@ int main(int argc, char* argv[])
   cout<<"Enter no of walks: "<<endl;
   cin>>walks;
 
-  //	omp_set_num_threads(num_threads);
+  //omp_set_num_threads(num_threads);
   double **conductance_array;
   double **probability_array;
-  double* **neighbor;
-  int **neighbor_id;
-  int *type;
+  double***neighbor;
+  int    **neighbor_id;
+  int    *type;
   double *current_value;
   double *node_potential;
   double *updated_potential;
   double *conductance_sum;
   double *reward;
-  int *no_neighbor;
+  int    *no_neighbor;
 
-  FILE * myReadFile;
+  FILE *myReadFile;
   char typ;
   int node1,node2,node3,k,max=-1;
   j=0;
@@ -60,16 +60,15 @@ int main(int argc, char* argv[])
     while(1)
     {
       fscanf(myReadFile, "%c%*d %d %d %lf\n",&typ,&node1,&node2,&value);
-	  if (typ=='.')
-	    break;
-
+      if (typ=='.')
+        break;
+	    
       node3=(node1>node2)?node1:node2;
-
-	  if(node3>max)
-	  {
-		max=node3;
-	  }
-	}
+      if(node3>max)
+      {
+        max=node3;
+      }
+    }
   }
   fclose(myReadFile);
 
@@ -87,10 +86,10 @@ int main(int argc, char* argv[])
 
   for(i=0;i<=max;i++)
   {
-  	neighbor[i]=(double**)calloc(4,sizeof(double*));
-	neighbor_id[i]=(int*)calloc(4,sizeof(int));
-	conductance_array[i]=(double*)calloc(4,sizeof(double));
-	probability_array[i]=(double*)calloc(4,sizeof(double));
+    neighbor[i]=(double**)calloc(4,sizeof(double*));
+    neighbor_id[i]=(int*)calloc(4,sizeof(int));
+    conductance_array[i]=(double*)calloc(4,sizeof(double));
+    probability_array[i]=(double*)calloc(4,sizeof(double));
   }
 
   int no1,no2;
@@ -104,33 +103,33 @@ int main(int argc, char* argv[])
       if (typ=='.')
         break;
       no1=no_neighbor[node1];
-	  no2=no_neighbor[node2];
+      no2=no_neighbor[node2];
 
       if(typ=='R')
-	  {
-		neighbor[node1][no1]=&reward[node2];
-		neighbor[node2][no2]=&reward[node1];
-		neighbor_id[node1][no1]=node2;
-		neighbor_id[node2][no2]=node1;
-		conductance_array[node1][no1]=1/value;
-		conductance_array[node2][no2]=1/value;
-		no_neighbor[node1]++;
-		no_neighbor[node2]++;
-	  }
+      {
+        neighbor[node1][no1]=&reward[node2];
+        neighbor[node2][no2]=&reward[node1];
+        neighbor_id[node1][no1]=node2;
+	neighbor_id[node2][no2]=node1;
+        conductance_array[node1][no1]=1/value;
+        conductance_array[node2][no2]=1/value;
+        no_neighbor[node1]++;
+        no_neighbor[node2]++;
+      }
 
       else if(typ=='V')
-	  {
-		node_potential[node1]=value;
-		type[node1]=1;
-	  }
+      {
+        node_potential[node1]=value;
+        type[node1]=1;
+      }
       else
-	  {
-		if(node2==0)
-		  current_value[node1]=value;
-		else
-		  current_value[node2]=-value;
-	  }
-	}
+      {
+        if(node2==0)
+          current_value[node1]=value;
+        else
+          current_value[node2]=-value;
+      }
+    }
   }
 
   fclose(myReadFile);
@@ -139,14 +138,14 @@ int main(int argc, char* argv[])
   cout<<"total node = "<<total_node<<endl;
 
   for(i=1;i<total_node;i++)
-	conductance_sum[i]=conductance_array[i][0]+conductance_array[i][1]+conductance_array[i][2]+conductance_array[i][3];
+    conductance_sum[i]=conductance_array[i][0]+conductance_array[i][1]+conductance_array[i][2]+conductance_array[i][3];
 
   for(i=1;i<total_node;i++)
   {
-	double temp=0;
-	for(j=0;j<no_neighbor[i];j++)
-	{
-	  probability_array[i][j]=temp+conductance_array[i][j]/conductance_sum[i];
+    double temp=0;
+    for(j=0;j<no_neighbor[i];j++)
+    {
+      probability_array[i][j]=temp+conductance_array[i][j]/conductance_sum[i];
       //cerr<<"probability "<<probability_array[i][j]<<endl;
       temp=probability_array[i][j];
     }
@@ -154,10 +153,10 @@ int main(int argc, char* argv[])
 
   for(i=1;i<total_node;i++)
   {
-	if(type[i]==1)
-	  reward[i]=node_potential[i];
-	else
-	  reward[i]=-current_value[i]/conductance_sum[i];
+    if(type[i]==1)
+      reward[i]=node_potential[i];
+    else
+      reward[i]=-current_value[i]/conductance_sum[i];
   }
 
   int l,iter,tot_iter=0,node_iter;
@@ -172,8 +171,8 @@ int main(int argc, char* argv[])
 
   for(i=1;i<total_node;i++)
   {
-    //   node_iter=0;
-	for(j=0;j<walks;j++)
+    //node_iter=0;
+    for(j=0;j<walks;j++)
     {
       tot_reward=reward[i];
       l=i;
@@ -185,24 +184,24 @@ int main(int argc, char* argv[])
         rand_no=rand_array[ind%10000000];
         //cerr<<" "<<i<<" neighbor "<<l<<" "<<rand_no<< endl;
         //cerr<<" "<<no_neighbor[l]<<endl;
-		for(k=0;k<no_neighbor[l];k++)
+        for(k=0;k<no_neighbor[l];k++)
         {
-		  if(rand_no<=probability_array[l][k])
-		  {
-		    break;
+          if(rand_no<=probability_array[l][k])
+	  {
+            break;
           }
         }
         //cerr<<" "<<k<<endl;
         tot_reward=tot_reward+*neighbor[l][k];
         //cout<<"neighbor["<<l<<"]["<<k<<"]="<<*neighbor[l][k]<<endl;
         //cerr<<" "<<iter<<" "<<tot_reward<<endl;
-		l=neighbor_id[l][k];
+        l=neighbor_id[l][k];
         ++iter;
-		++ind;
+        ++ind;
       }
       updated_potential[i]=updated_potential[i]+tot_reward;
-	}
-	node_potential[i]=updated_potential[i]/walks;
+    }
+    node_potential[i]=updated_potential[i]/walks;
     type[i]=1;
     reward[i]=node_potential[i];
   }
